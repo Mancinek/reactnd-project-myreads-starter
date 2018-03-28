e843026c284c4917f6ae7e7adefd9732868a94e6
Marcin Bednarz <marcin.bednarz@allegrogroup.com>
2018-03-28 19:58:20 +0200

    refactor: Listbooks as SFC
    Listbooks component has been refactored to Stateless Functional Component, cause it had only render method

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,32 +3,25 @@ import './App.css'
 import Bookshelf from './Bookshelf.js'
 import { Link } from 'react-router-dom'
 
-// stateless!!!!!!!!!!!!!!
-class ListBooks extends Component {
+const ListBooks = (props) => (
+    <div className="app">
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <Bookshelf id={'currentlyReading'} title={'Currently Reading'} booksOnShelf={props.books} onChangeShelf={props.onUpdateBooks}/>
+                    <Bookshelf id={'wantToRead'} title={'Want to Read'} booksOnShelf={props.books} onChangeShelf={props.onUpdateBooks}/>
+                    <Bookshelf id={'read'} title={'Read'} booksOnShelf={props.books} onChangeShelf={props.onUpdateBooks}/>
+                </div>
+            </div>
 
-    render() {
-        const {books, onUpdateBooks} = this.props; // decomposition
-        return (
-            <div className="app">
-                <div className="list-books">
-                    <div className="list-books-title">
-                        <h1>MyReads</h1>
-                    </div>
-                    <div className="list-books-content">
-                        <div>
-                            <Bookshelf id={'currentlyReading'} title={'Currently Reading'} booksOnShelf={books} onChangeShelf={onUpdateBooks}/>
-                            <Bookshelf id={'wantToRead'} title={'Want to Read'} booksOnShelf={books} onChangeShelf={onUpdateBooks}/>
-                            <Bookshelf id={'read'} title={'Read'} booksOnShelf={books} onChangeShelf={onUpdateBooks}/>
-                        </div>
-                    </div>
-
-                    <div className="open-search">
-                        <Link to='/search'>Add a book</Link>
-                    </div>
-              </div>
-          </div>
-        )
-  }
-}
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+      </div>
+  </div>
+)
 
 export default ListBooks
